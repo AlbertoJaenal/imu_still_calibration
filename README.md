7eb3e86bdad823a8e247cfed4c2eac459da39e0c
Alberto en Bishop <alberto.jaenal@gmail.com>
2019-03-19 09:02:34 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,128 @@
-# imu_still_calibration
+# IMU Still Calibration
+
+Inertial measurements can be used for a various applications, as pure inertial odometry or Visual-Inertial SLAM methods. As cameras, Inertial Measurement Units must be well calibrated to correctly estimate the movements of a the sensor.
+
+This repository contains a calibration tool based on **Python** for IMUs, following the model presented in [The TUM VI Benchmark for Evaluating Visual-Inertial Odometry](https://arxiv.org/pdf/1804.06120.pdf). The tool models the bias and random walk parameters of a still IMU through Allan Variances, and then estimates them. For a sufficient calibration, it is recommended to store measures during large periods of time (> 80 h).
+
+If you use this code in your research, we would appreciate if you cite the respective publication.
+
+**A Dataset for Visual Inertial Odometry in Challenging Environments with Low-texture and HDR Illuminations**
+(D. Zuñiga-Noël, A. Jaenal, R. Gomez-Ojeda, J Gonzalez-Jimenez),
+*In *, .
+[[pdf]()]
+
+# Usage
+
+The code is based on Python 3, using default libraries as *numpy* and *matplotlib*.
+
+```
+Example:
+python calibrate_imu.py /mnt/data/UMAVI/measures.npy -f=250
+```
+
+```
+usage: calibrate_imu.py [-h] [-f FREQUENCY] [-w WINDOWS] [-n NUM_SAMPLES]
+                        [-o DEVORDER] [-a ACCVARS] [-g GYROVARS] [-v]
+                        [--limWlow LIMWLOW] [--limWupp LIMWUPP]
+                        [--limBlow LIMBLOW] [--limBupp LIMBUPP]
+                        numpy_path
+
+positional arguments:
+  numpy_path            Path to a .npy file for the dataset
+
+optional arguments:
+  -h, --help                                        show this help message and exit
+  -v, --verbose
+  -f FREQUENCY, --frequency FREQUENCY               Sampling frequency (default: 200.0 Hz)
+  -w WINDOWS, --windows WINDOWS                     Minimum windows for the Tau loop (default: 30)
+  -n NUM_SAMPLES, --num_samples NUM_SAMPLES         Number of samples for the Allan Computation(default: 2200)
+  -o DEVORDER, --devOrder DEVORDER                  Device saving order: 0 - Gyro/Acc (default), 1 - Acc/Gyro
+  -a ACCVARS, --accVars ACCVARS                     Accelerometer variables used for the printing. Ex: xyz, xz
+  -g GYROVARS, --gyroVars GYROVARS                  Gyroscope variables used for the printing. Ex: xyz, xz
+  --limWlow LIMWLOW     Minimum integration time for the bias_w (default: 0.02 s)
+  --limWupp LIMWUPP     Maximum integration time for the bias_w (default: 1 s)
+  --limBlow LIMBLOW     Minimum integration time for the bias_b (default: 1000 s)
+  --limBupp LIMBUPP     Maximum integration time for the bias_b (default: 6000 s)
+
+```
+
+## Input
+
+The input file should be a .npy file with the next size and format:
+```
+Size: 
+[num_samples x 7 (8)] 
+
+Format: 
+# Example: timestamp, gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, (temperature)   -->   --devOrder=0
+OR
+# Example: timestamp, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, (temperature)   -->   --devOrder=1
+```
+
+The order of the sensor measurements is indicated by the flag *devOrder*. The temperature is not taken care of yet, so it is between brackets and it is not necessary to include it into the .npy file.
+
+## Output
+
+The script outputs an image with the graphic (as the shown) and a .yaml file with the Noise density and Biases for each device.
+As an intermediate step, the script outputs a file (.../npypath_AllanVar.npy) which contains all the integrated measures for a fixed frequency to avoid waiting large periods of time if we want to change the display parameters (integration time limits, printing variables).
+
+### UMA Visual Inertial Dataset Calibration
+
+The IMU still calibration (Xsens MTi-28A53G35) from the [UMA VI dataset](http://mapir.isa.uma.es/mapirwebsite) has the next input:
+
+As remarkable parameters, in the UMA VI dataset the frequency is 250 Hz and the integration time limits are changed to fit with the more "linear" part (0.02-1 s, 10000-10000 s).
+
+```
+python3 calibrate_imu.py ./results/UMAvi_imu_still_2019-02-15-16-49-53.npy -f=250 -w=30 -n=2200 --limBlow=10000 --limBupp=100000
+```
+
+![](./results/UMAvi_imu_still_2019-02-15-16-49-53.png)
+
+```
+#Accelerometers
+accelerometer_noise_density: 0.0007970737515806496 #Noise density (continuous-time)
+accelerometer_random_walk: 4.134509826460877e-06 #Bias random walk
+
+#Gyroscopes
+gyroscope_noise_density: 0.00046306229509090514 #Noise density (continuous-time)
+gyroscope_random_walk: 9.798339194666108e-07 #Bias random walk
+
+rostopic: /imu0/data #the IMU ROS topic
+update_rate: 250.0 #Hz (for discretization of the values above)
+```
+
+### TUM Visual Inertial Dataset IMU Calibration
+
+The IMU still calibration (BMI160) from the [TUM VI dataset (calib-imu-static2)](https://vision.in.tum.de/data/datasets/visual-inertial-dataset) has the next input:
+
+As remarkable parameters, in the TUM VI dataset paper is shown that for the gyroscope they only evaluate the measures for the axes y and z (-g=yz) and the integration time limits are the defaults (0.02-1 s, 1000-6000 s).
+
+```
+python3 calibrate_imu.py ./results/TUMvi_still_imu.npy -f=200 -w=30 -n=2200 -g=yz
+```
+
+The script output is the next (the results of TUM can be visualized on their page, being quite similar):
+![](./results/TUMvi_still_imu.png)
+
+```
+#Accelerometers
+accelerometer_noise_density: 0.0013588692876997399 #Noise density (continuous-time)
+accelerometer_random_walk: 8.157817021466894e-05 #Bias random walk
+
+#Gyroscopes
+gyroscope_noise_density: 8.078661856971916e-05 #Noise density (continuous-time)
+gyroscope_random_walk: 2.1948834995716578e-06 #Bias random walk
+
+rostopic: /imu0/data #the IMU ROS topic
+update_rate: 200.0 #Hz (for discretization of the values above)
+```
+
+
+## Memory comsumption
+**WARNING:** the script is executed on RAM and can saturate it. The --num_samples flag purpose is to decrease the interpolation points of the IMU integrated response curve, which will free the RAM obtaining a lower precision.
+
+# License
+
+This project has been developed by Alberto Jaenal at the [MAPIR group](http://mapir.uma.es/mapirwebsite/) in 2019, being currently maintained by its author.
+
+This project is available under a BSD 3-Clause license. See [LICENSE.txt](LICENSE.txt)
---
+++ calibrate_imu.py
@@ -0,0 +1,209 @@
+import os
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('numpy_path', help='Path to a .npy file for the dataset')
+
+    parser.add_argument("-f", "--frequency", help="Sampling frequency (default: 200.0 Hz)", type=float, default=200.0)
+    parser.add_argument("-w", "--windows", help="Minimum windows for the Tau loop (default: 30)", type=int, default=30)
+    parser.add_argument("-n", "--num_samples", help="Number of samples for the Allan Computation(default: 2200)",
+                        type=int, default=2200)
+    parser.add_argument("-o", "--devOrder", help="Device saving order: 0 - Gyro/Acc (default), 1 - Acc/Gyro", type=int,
+                        default=0)
+
+    parser.add_argument("-a", "--accVars", help="Accelerometer variables used for the printing. Ex: xyz, xz",
+                        default='xyz')
+    parser.add_argument("-g", "--gyroVars", help="Gyroscope variables used for the printing. Ex: xyz, xz",
+                        default='xyz')
+
+    parser.add_argument("-v", "--verbose", action='store_true', default='xyz')
+
+    parser.add_argument("--limWlow", help="Minimum integration time for the bias_w (default: 0.02 s)", type=float,
+                        default=0.02)
+    parser.add_argument("--limWupp", help="Maximum integration time for the bias_w (default: 1 s)", type=float,
+                        default=1)
+    parser.add_argument("--limBlow", help="Minimum integration time for the bias_b (default: 1000 s)", type=float,
+                        default=1000.0)
+    parser.add_argument("--limBupp", help="Maximum integration time for the bias_b (default: 6000 s)", type=float,
+                        default=6000.0)
+    args = parser.parse_args()
+
+
+    ##################################################
+    ### ALLAN VARIANCE COMPUTATION                 ###
+    ## Check if the file has been previously crated ##
+    ##################################################
+
+    TAU_0 = 1 / args.frequency
+    num = args.num_samples
+    # For each [x,y,z]
+    # Example: timestamp, gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, (temperature)
+    if args.devOrder == 0:
+        print(' \nSetting Gyroscope-Accelerometer as data order...\n')
+        DATA_AXES = {'Acc': [4, 5, 6], 'Gyro': [1, 2, 3]}
+    else:
+        print(' \nSetting Accelerometer-Gyroscope as data order...\n')
+        DATA_AXES = {'Acc': [1, 2, 3], 'Gyro': [4, 5, 6]}
+
+    if not os.path.exists(args.numpy_path.replace('.npy', '_AllanVar.npy')):
+        data = np.load(args.numpy_path)
+        M = data.shape[0]
+
+        ## Loop to obtain a logarithmic scale for the x axis with num non-repeated numbers
+        f = True
+        n = num
+        while f:
+            f = (len(np.unique(np.logspace(0, np.log10((M - args.windows) // 2), num=n, dtype=np.int))) < num)
+            n += 10
+
+        entries = np.unique(np.logspace(0, np.log10((M - args.windows) // 2), num=n, dtype=np.int))
+        num = len(entries)
+
+        ###############################
+        # Tau (integration time) loop #
+        ###############################
+        variances = np.empty([num, 7])
+        # The variables must be placed from the axis 1. The axis 0 is reserved for the timestamps (not used in this algorithm)
+        ret = np.cumsum(data[:, 1:7], axis=0)
+        # Deletes the data in order to save space
+        del data
+        f = time.time()
+
+        for i, n in enumerate(entries):
+            # Optimum OVERLAPPING computation
+            variances[i, 1:] = 1 / (2 * (M - 2 * n + 1)) * (
+                    (ret[2 * n:, :] - 2 * ret[n:-n, :] + ret[:-2 * n, :]) ** 2).sum(axis=0) / (n ** 2)
+            if args.verbose:
+                print(i, n, (M - args.windows) // 2, time.time() - f, )
+            f = time.time()
+
+        # Store also the integration numbers (n), not tau
+        variances[:, 0] = entries[:]
+        np.save(args.numpy_path.replace('.npy', '_AllanVar.npy'), variances)
+
+        # Deletes the data in order to save space
+        del ret
+    else:
+        variances = np.load(args.numpy_path.replace('.npy', '_AllanVar.npy'))
+
+    #########################################
+    ### BIASES COMPUTATION                ###
+    #########################################
+    # log(y) = m*log(x) + b                 #
+    # Minimize: m*log(x) - log(y) = -b      #
+    # SIGMA_W = 10^(-1/2*log(1)+optBias_W)  #
+    # SIGMA_B = 10^(1/2*log(3)+optBias_B)   #
+    #########################################
+    # Obtain the indexes of the indicated time intervals
+    indW = np.argwhere(np.logical_and(variances[:, 0] * TAU_0 <= args.limWupp, variances[:, 0] * TAU_0 >= args.limWlow))
+    indB = np.argwhere(np.logical_and(variances[:, 0] * TAU_0 <= args.limBupp, variances[:, 0] * TAU_0 >= args.limBlow))
+
+    # Create vectors with the indicated variables (x, y and/or z) for each device
+    xAccW, yAccW, xAccB, yAccB = [], [], [], []
+    for i, v in enumerate(['x', 'y', 'z']):
+        if v in args.accVars:
+            xAccW.append(variances[indW, 0])
+            yAccW.append(variances[indW, DATA_AXES['Acc'][i]])
+            xAccB.append(variances[indB, 0])
+            yAccB.append(variances[indB, DATA_AXES['Acc'][i]])
+
+    xGyroW, yGyroW, xGyroB, yGyroB = [], [], [], []
+    for i, v in enumerate(['x', 'y', 'z']):
+        if v in args.gyroVars:
+            xGyroW.append(variances[indW, 0])
+            yGyroW.append(variances[indW, DATA_AXES['Gyro'][i]])
+            xGyroB.append(variances[indB, 0])
+            yGyroB.append(variances[indB, DATA_AXES['Gyro'][i]])
+
+    # Create the numpy arrays for the Least Squares problem
+    # NOTE: it is necesary to obtain the Allan Deviation and the integration time in seconds
+    xAccW = np.concatenate(xAccW) * TAU_0
+    yAccW = np.sqrt(np.concatenate(yAccW))
+    xAccB = np.concatenate(xAccB) * TAU_0
+    yAccB = np.sqrt(np.concatenate(yAccB))
+
+    xGyroW = np.concatenate(xGyroW) * TAU_0
+    yGyroW = np.sqrt(np.concatenate(yGyroW))
+    xGyroB = np.concatenate(xGyroB) * TAU_0
+    yGyroB = np.sqrt(np.concatenate(yGyroB))
+
+    # Obtain the biases by a minimization problem
+    biasAccW = 10 ** (
+    np.linalg.lstsq(-np.ones_like(xAccW), np.squeeze(np.log10(xAccW) * (-1 / 2) - np.log10(yAccW)))[0])
+    biasAccB = 10 ** (np.linalg.lstsq(-np.ones_like(xAccB), np.squeeze(np.log10(xAccB) * (1 / 2) - np.log10(yAccB)))[
+                          0] + np.log10(3) / 2)
+    biasGyroW = 10 ** (
+    np.linalg.lstsq(-np.ones_like(xGyroW), np.squeeze(np.log10(xGyroW) * (-1 / 2) - np.log10(yGyroW)))[0])
+    biasGyroB = 10 ** (
+            np.linalg.lstsq(-np.ones_like(xGyroB), np.squeeze(np.log10(xGyroB) * (1 / 2) - np.log10(yGyroB)))[
+                0] + np.log10(3) / 2)
+
+    # Print the results
+    print('\n\n\nAcelerometer:\nSigma_w: ', biasAccW, '\nSigma_b: ', biasAccB)
+    print('Gyroscope:\nSigma_w: ', biasGyroW, '\nSigma_b: ', biasGyroB, '\n\n\n')
+
+    with open(args.numpy_path.replace('.npy','.yaml'), 'w') as f:
+        f.write('#Accelerometers\n')
+        f.write('accelerometer_noise_density: ' + str(biasAccW.squeeze()) + ' #Noise density (continuous-time)\n')
+        f.write('accelerometer_random_walk: ' + str(biasAccB.squeeze()) + ' #Bias random walk\n\n')
+
+        f.write('#Gyroscopes\n')
+        f.write('gyroscope_noise_density: ' + str(biasGyroW.squeeze()) + ' #Noise density (continuous-time)\n')
+        f.write('gyroscope_random_walk: ' + str(biasGyroB.squeeze()) + ' #Bias random walk\n\n')
+
+        f.write('rostopic: /imu0/data #the IMU ROS topic\n')
+        f.write('update_rate: ' + str(args.frequency) + ' #Hz (for discretization of the values above)\n')
+
+    ############
+    # PLOTTING #
+    ############
+    # X coordinates for the regressed lines
+    xW = [args.limWlow, args.limWupp, 1]
+    xB = [3, args.limBlow, args.limBupp]
+
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+
+    # Plot the curves for the Accelerometer
+    ax[0].plot(variances[:, 0] * TAU_0, np.sqrt(variances[:, DATA_AXES['Acc'][0]]), label='x')
+    ax[0].plot(variances[:, 0] * TAU_0, np.sqrt(variances[:, DATA_AXES['Acc'][1]]), label='y')
+    ax[0].plot(variances[:, 0] * TAU_0, np.sqrt(variances[:, DATA_AXES['Acc'][2]]), label='z')
+    # Plot the biases
+    ax[0].plot(1, biasAccW, 'g*', label='sig_b', markersize=10)  # ='+str(round(float(biasAccW),6)))
+    ax[0].plot(3, biasAccB, 'r*', label='sig_w', markersize=10)  # ='+str(round(float(biasAccB),6)))
+    # Plot the regressed lines
+    ax[0].plot(np.array(xW), 10 ** (np.log10(biasAccW) - np.log10(xW) / 2), 'k', marker='o', linestyle='--',
+               markersize=3)
+    ax[0].plot(np.array(xB), 10 ** (np.log10(biasAccB) + np.log10(xB) / 2 - np.log10(3) / 2), 'k', marker='o',
+               linestyle='--', markersize=3)
+    # Plot settings
+    ax[0].set_title('Accelerometer Allan Deviation')
+    ax[0].set_xscale('log')
+    ax[0].set_yscale('log')
+    ax[0].grid()
+    ax[0].legend()
+
+    # Plot the curves for the Gyroscope
+    ax[1].plot(variances[:, 0] * TAU_0, np.sqrt(variances[:, DATA_AXES['Gyro'][0]]), label='x')
+    ax[1].plot(variances[:, 0] * TAU_0, np.sqrt(variances[:, DATA_AXES['Gyro'][1]]), label='y')
+    ax[1].plot(variances[:, 0] * TAU_0, np.sqrt(variances[:, DATA_AXES['Gyro'][2]]), label='z')
+    # Plot the biases
+    ax[1].plot(1, biasGyroW, 'g*', label='sig_b', markersize=10)  # ='+str(round(float(biasGyroW),6)))
+    ax[1].plot(3, biasGyroB, 'r*', label='sig_w', markersize=10)  # ='+str(round(float(biasGyroB),6)))
+    # Plot the regressed lines
+    ax[1].plot(np.array(xW), 10 ** (np.log10(biasGyroW) - np.log10(xW) / 2), 'k', marker='o', linestyle='--',
+               markersize=3)
+    ax[1].plot(np.array(xB), 10 ** (np.log10(biasGyroB) + np.log10(xB) / 2 - np.log10(3) / 2), 'k', marker='o',
+               linestyle='--', markersize=3)
+    # Plot settings
+    ax[1].set_title('Gyroscope Allan Deviation')
+    ax[1].set_xscale('log')
+    ax[1].set_yscale('log')
+    ax[1].grid()
+    ax[1].legend()
+
+    fig.savefig(args.numpy_path.replace('.npy', '.png'))
+    plt.show()
